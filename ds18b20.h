b6ade1ca7fa8b8ffe89d9176406d92e6fd90b687
QinYuan <qyts1989@163.com>
2024-12-18 19:17:10 +0800

    add ds18b20.h

---
+++ ds18b20.h
@@ -0,0 +1,9 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
